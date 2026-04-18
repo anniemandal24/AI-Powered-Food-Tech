c8793bdbf47efaafa4694443392a924155698610
anniemandal24 <anniemandal24@gmail.com>
2026-04-18 21:24:50 +0530

    crudfunc

--- Backend/core-api/src/controllers/item.controller.ts
+++ Backend/core-api/src/controllers/item.controller.ts
@@ -4,4 +4,109 @@ import { user } from "../models/user.model.js";
 import { item } from "../models/item.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-
+import mongoose from "mongoose";
+import { uploadOnCloudinary,deleteFromCloudinary } from "../services/cloudinary.js";
+export const addItem=asyncHandler(async(req,res)=>{
+    const{name,category,quantity,expiryDate,isEstimatedExpiry,source}=req.body
+    if(!name||!expiryDate||!source){
+        throw new ApiError(400,"name,expirydate and source required")
+    }
+    if(!["MANUAL","IMAGE","PDF"].includes(source)){
+        throw new ApiError(400,"source should be manual,image and pdf")
+    }
+    const newItem=await item.create({user:req.user._id,name,category,quantity,expiryDate,isEstimatedExpiry:isEstimatedExpiry||false,source,status:"AVAILABLE"})
+    res.status(201).json(new ApiResponse(201,newItem,"Item added"))
+})
+export const getAllItem=asyncHandler(async(req,res)=>{
+    const items=await item.find({user:req.user._id})
+    res.status(200).json(new ApiResponse(200,items,"Items fetched"))
+})
+export const getItembyStatus=asyncHandler(async(req,res)=>{
+    const {status}=req.params
+    const statustr=status as String
+    if(!status){
+        throw new ApiError(404,"status not found")
+    }
+    if(!["AVAILABLE","CONSUMED","WASTED"].includes(statustr.toUpperCase())){
+        throw new ApiError(404,"status must be available,consumed or wasted")
+    }
+    const items=await item.find({user:req.user._id,status:statustr.toUpperCase()})
+    res.status(200).json(new ApiResponse(200,items,"${status} items fetched successfully"))
+})
+export const getItemById=asyncHandler(async(req,res)=>{
+    const id=req.params.id as string
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        throw new ApiError(400,"Invalid item")
+    }
+    const foodItem=await item.findOne({_id:new mongoose.Types.ObjectId(id),user:req.user._id})
+    if(!foodItem){
+        throw new ApiError(404,"Item not found")
+    }
+    res.status(200).json(new ApiResponse(200,foodItem,"Item fetched"))
+})
+export const editItem=asyncHandler(async(req,res)=>{
+    const {name,category,quantity,expiryDate,isEstimatedExpiry}=req.body
+    const id=req.params.id as string
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        throw new ApiError(400,"Invalid item")
+    }
+    const foodItem=await item.findOneAndUpdate({_id:new mongoose.Types.ObjectId(id),user:req.user._id},
+    {
+        $set:{name,category,quantity,expiryDate,isEstimatedExpiry}
+    },{new:true})
+    if(!foodItem){
+        throw new ApiError(404,"Item not found")
+    }
+    res.status(200).json(new ApiResponse(200,foodItem,"Item Updated successfully"))
+})
+export const updateItemStatus=asyncHandler(async(req,res)=>{
+    const {status}=req.body;
+    const statustr=status as string
+    const id=req.params.id as string
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        throw new ApiError(400,"Invalid item")
+    }
+    if(!["AVAILABLE","CONSUMED","WASTED"].includes(statustr.toUpperCase())){
+        throw new ApiError(400,"status must be AVAILABLE,CONSUMED or WASTED")
+    }
+    const foodItem=await item.findOneAndUpdate({_id:new mongoose.Types.ObjectId(id),user:req.user._id},
+    {
+        $set:{status,actionDate:new Date()}
+    },{new:true})
+    if(!foodItem){
+        throw new ApiError(404,"Item not found")
+    }
+    res.status(200).json(new ApiResponse(200,foodItem,"Item marked as ${status}"))
+    
+})
+export const deleteItem=asyncHandler(async(req,res)=>{
+    const id=req.params.id as string
+    const foodItem=await item.findOneAndDelete({_id:new mongoose.Types.ObjectId(id),user:req.user.id})
+    if(!foodItem){
+        throw new ApiError(404,"Item not found")
+    }
+    res.status(200).json(new ApiResponse(200,null,"Item deleted"))
+})
+export const getExpiredItems=asyncHandler(async(req,res)=>{
+    const expiredItems=await item.find({user:req.user._id,expiryDate:{$lt:new Date()},status:"AVAILABLE"})
+    res.status(200).json(new ApiResponse(200,expiredItems,"Expired Items fetched"))
+})
+export const uploadFile=asyncHandler(async(req,res)=>{
+    const localFilePath=req.file?.path;
+    if(!localFilePath){
+        throw new ApiError(404,"No File provided")
+    }
+    const cloudinaryResponse=await uploadOnCloudinary(localFilePath)
+    if(!cloudinaryResponse){
+        throw new ApiError(500,"Failed to upload file")
+    }
+    res.status(200).json(new ApiResponse(200,{url:cloudinaryResponse.url,public_id:cloudinaryResponse.public_id},"File uploaded successfully"))
+})
+export const deleteFile=asyncHandler(async(req,res)=>{
+    const public_id=req.params.public_id as string
+    if(!public_id){
+        throw new ApiError(404,"public_id is required")
+    }
+    await deleteFromCloudinary(public_id)
+    res.status(200).json(new ApiResponse(200,null,"deleted successfully"))
+})
--- Backend/core-api/src/controllers/user.controller.ts
+++ Backend/core-api/src/controllers/user.controller.ts
@@ -34,13 +34,70 @@ export const addFamilyMembers = asyncHandler(async (req:Request, res:Response)=>
 })
 
 export const getUserProfile = asyncHandler(async (req:Request, res:Response)=>{
-    
+    const foundUser=await user.findById(req.user._id).select("-passwordHash -refreshToken")
+    if(!foundUser){
+        throw new ApiError(404,"User not found")
+
+    }
+    res.status(200).json(new ApiResponse(200,foundUser,"Profile fetched successfully"))
 })
 
 export const updateProfile = asyncHandler(async (req:Request, res:Response)=>{
-
+    const {name}=req.body;
+    if(!name){
+        throw new ApiError(400,"Name is required")
+    }
+    const updateUser=await user.findByIdAndUpdate(req.user._id,{$set:{name}},{new:true}).select("-passwordHash -refreshToken")
+    if(!updateUser){
+        throw new ApiError(404,"user not found")
+    }
+    res.status(200).json(new ApiResponse(200,updateUser,"Profile updated successfully"))
 })
 
-export const getInventory = asyncHandler(async (req:Request, res:Response)=>{
-
+export const editFamilyMember = asyncHandler(async (req:Request, res:Response)=>{
+    const {memberIndex}=req.params
+    const{name,ageGroup,gender,dietaryPractices,healthStatus}=req.body
+    const foundUser=await user.findById(req.user._id)
+    if(!foundUser){
+        throw new ApiError(404,"User not found")
+    }
+    const index=Number(memberIndex)
+    const member=foundUser.familyMembers[index]
+    if(index<0 || index>=foundUser.familyMembers.length){
+        throw new ApiError(400,"Invalid Family Member Index")
+    }
+    if (!member) {
+        throw new ApiError(400, "Family member not found at this index");
+    }
+    if(name){
+        member.name=name
+    }
+    if(ageGroup){
+        member.ageGroup=ageGroup
+    }
+    if(gender){
+        member.gender=gender
+    }
+    if(dietaryPractices){
+        member.dietaryPractices=dietaryPractices
+    }
+    if(healthStatus){
+        member.healthStatus=healthStatus
+    }
+    await foundUser.save({validateBeforeSave:false})
+    res.status(200).json(new ApiResponse(200,foundUser.familyMembers,"Family Member Updated Successfully"))
+})
+export const removeFamilyMember=asyncHandler(async(req,res)=>{
+    const {memberIndex}=req.params
+    const foundUser=await user.findById(req.user._id)
+    if(!foundUser){
+        throw new ApiError(404,"user not found")
+    }
+    const index=Number(memberIndex)
+    if (index < 0 || index >= foundUser.familyMembers.length) {
+        throw new ApiError(400, "Invalid family member index")
+    }
+    foundUser.familyMembers.splice(index,1);
+    await foundUser.save({validateBeforeSave:false})
+    res.status(200).json(new ApiResponse(200,foundUser.familyMembers,"family member removed successfully"))
 })
