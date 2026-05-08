dfd41267adcfa9e0cf2374dd764fe6faed492c02
SjB <shubhajitb66@gmail.com>
2026-05-09 03:10:07 +0530

    in nav bar Get Started Free button will not show after login

--- Frontend/src/components/layout/Navbar.jsx
+++ Frontend/src/components/layout/Navbar.jsx
@@ -6,6 +6,7 @@ import Impact from '../home/Impact';
 import Testimonials from '../home/Testimonials';
 import Hero from '../home/Hero';
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import NotificationBell from "../home/notificationBell";
 
 <Link to="/chat">AI Chat</Link>
@@ -31,6 +32,25 @@ export default function Navbar() {
     const section = document.getElementById("hero");
     section.scrollIntoView({ behavior: "smooth" });
   };
+
+  const navigate = useNavigate();
+  const token = localStorage.getItem("accessToken");
+  // 🔐 Handle Scan Button Click (with auth check)
+  const handleScanClick = () => {
+
+    const token = localStorage.getItem("accessToken");
+    if (token && token !== "undefined") {
+      console.log("Auth verified: Navigating to Inventory");
+      navigate("/inventory")
+    }
+    else {
+      console.log("No token found: Redirecting to Login");
+      // Use the 'state' property so the user is sent back here after logging in
+      navigate("/login-signup", { state: { from: "/inventory" } });
+    }
+  };
+
+
   return (
     <nav className="nav">
       <div className="logo">
@@ -45,7 +65,9 @@ export default function Navbar() {
         <li><a onClick={handleScroll4}>Reviews</a></li>
         <li><NotificationBell /></li>
       </div>
-      <button className='nav-cat'>Get Started Free</button>
+      {!token || token === "undefined" ? (
+        <button className='nav-cat' onClick={handleScanClick}>Get Started Free</button>
+      ) : null}
     </nav>
   );
 }
